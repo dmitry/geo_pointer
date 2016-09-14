96c3f85ace827b5a9712662597274533cef9a757
Dmitry Polushkin <dmitry.polushkin@gmail.com>
2016-09-14 12:27:30 +0100

    Update specs with new changes

--- spec/overpass_spec.rb
+++ spec/overpass_spec.rb
@@ -1,49 +1,44 @@
 require 'spec_helper'
 
 require 'overpass'
+require 'oj'
 
 describe 'hierarchy of' do
   it 'hamburg', :vcr => true do
-    response = Overpass.get_geojson(53.5642280, 9.9830910)
-    expect(response.size).to eq 3480635
-    json = JSON.parse(response)
+    json = Overpass.get_geojson(53.5642280, 9.9830910)
+    expect(Oj.dump(json, mode: :compat).size).to eq 3480626
     expect(json['type']).to eq 'FeatureCollection'
     expect(json['features'].size).to eq 5
     expect(json['features'].first['properties']['translations'].size).to eq 252
   end
 
   it 'puerto de la cruz', :vcr => true do
-    response = Overpass.get_geojson(28.41, -16.54)
-    json = JSON.parse(response)
+    json = Overpass.get_geojson(28.41, -16.54)
     expect('FeatureCollection').to eq json['type']
     expect(json['features'].size).to eq 4
   end
 
   it 'dublin', :vcr => true do
-    response = Overpass.get_geojson(53.397187, -6.426769)
-    json = JSON.parse(response)
+    json = Overpass.get_geojson(53.397187, -6.426769)
     expect('FeatureCollection').to eq json['type']
     expect(json['features'].size).to eq 6
   end
 
   it 'dublin without historic', :vcr => true do
-    response = Overpass.get_geojson(53.338893, -6.239656)
-    json = JSON.parse(response)
+    json = Overpass.get_geojson(53.338893, -6.239656)
     expect('FeatureCollection').to eq json['type']
     expect(json['features'].map { |v| v['properties']['name'] }).to eq(["Ireland", "Leinster", "County Dublin", "Dublin", "South Dock ED"])
     expect(json['features'].size).to eq 5
   end
 
   it 'seychelles', :vcr => true do
-    response = Overpass.get_geojson(-4.6261077, 55.446254)
-    json = JSON.parse(response)
+    json = Overpass.get_geojson(-4.6261077, 55.446254)
     expect('FeatureCollection').to eq json['type']
     expect(json['features'].size).to eq 2
   end
 
   it 'barcelona', :vcr => true do
-    response = Overpass.get_geojson(41.381003,2.146518)
-    json = JSON.parse(response)
+    json = Overpass.get_geojson(41.381003,2.146518)
     expect('FeatureCollection').to eq json['type']
     expect(json['features'].size).to eq 7
   end
